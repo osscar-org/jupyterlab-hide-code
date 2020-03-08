dd5e124b9627ce5b75cd147142f6278dc4eb624c
Dou <dou.du.epfl@gmail.com>
2020-03-08 15:36:52 +0100

    Remove some codes

--- src/index.ts
+++ src/index.ts
@@ -37,8 +37,6 @@ class ButtonExtension implements DocumentRegistry.IWidgetExtension<NotebookPanel
   
     let hideInputCode = () => {
       NotebookActions.runAll(panel.content, context.session);   
-      /**      NotebookActions.hideAllCode(panel.content); */
-      // alert("All the codes will be hidden !"); 
 
       panel.content.widgets.forEach(cell => {
         if (cell.model.type === 'code'){
@@ -50,7 +48,6 @@ class ButtonExtension implements DocumentRegistry.IWidgetExtension<NotebookPanel
       buttonShowInput.show(); 
     };
 	let showInputCode = () => {
-    /**  NotebookActions.showAllCode(panel.content); */ 
 
       panel.content.widgets.forEach(cell => {
         if (cell.model.type === 'code'){
@@ -87,48 +84,12 @@ class ButtonExtension implements DocumentRegistry.IWidgetExtension<NotebookPanel
           name: string; 
       }
 
-// Create a comm from the client side.
-  const session = panel.session; 
-
-  session.ready.then(() =>{ 
-
-      let kernel = session.kernel; 
-  
-  kernel.ready.then(() =>{
-
-  kernel.registerCommTarget('test2', (comm, commMsg) => {
-      if (commMsg.content.target_name !== 'test2') {
-        return;
-      }
-      comm.onMsg = msg => {
-        console.log(msg); // 'hello'
-        let msgData = (msg.content.data as unknown) as testMessageData;
-
-          if (msgData.type === 'hide'){
-              buttonHideInput.hide();
-          }
-          else if (msgData.type === 'show'){
-              buttonHideInput.show();
-          }
-      };
-      comm.onClose = msg => {
-        console.log(msg); // 'bye'
-      };
-  });
-  });
-  });
-
-
-
-
-
     return new DisposableDelegate(() => {
       buttonHideInput.dispose();
 	  buttonShowInput.dispose();
     });
   }
 
-  
 }
 
 function activate(app: JupyterFrontEnd) {
